e05dcdfc5862c7740346e20b8cf4574070234b3f
myhilmyhill <44314051+myhilmyhill@users.noreply.github.com>
2025-05-14 19:29:56 +0900

    remove deprecated

--- app/main.py
+++ app/main.py
@@ -1,26 +1,17 @@
 from typing import Annotated
-from fastapi import FastAPI, Request, Depends, Body, Path
-from starlette.responses import RedirectResponse
+from fastapi import FastAPI, Request
 from starlette.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
-from pydantic import BaseModel
 from datetime import datetime
-import os
 from .dependencies import DbConnectionDep
 from .routers import api
-from .smb import SMB
-from sqlite3 import Connection
 
 app = FastAPI()
 app.include_router(api.router)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
-
-smb_server = os.environ["smb_server"]
-dst_root = os.environ["dst_root"]
-smb = SMB(smb_server, os.environ["smb_username"], os.environ["smb_password"])
 
 @app.get("/", response_class=HTMLResponse)
 def digestions(request: Request, con: DbConnectionDep):
@@ -116,96 +107,3 @@ def views(request: Request, con: DbConnectionDep):
     views = cur.fetchall()
     return templates.TemplateResponse(
         request=request, name="views.html", context={"views": views})
-
-
-class Program(BaseModel):
-    event_id: int
-    service_id: int
-    name: str
-    start_time: datetime
-    duration: int
-
-def get_or_create_program(con: Connection, program: Program, created_at: datetime, viewed_time: datetime) -> int:
-    cur = con.cursor()
-    cur.execute("""
-        SELECT id, duration, created_at AS "created_at [timestamp]"
-        FROM programs
-        WHERE event_id = ? AND service_id = ? AND start_time = ?
-    """, (
-        program.event_id,
-        program.service_id,
-        program.start_time
-    ))
-    row = cur.fetchone()
-
-    if row:
-        id, duration, c = row
-        if duration != program.duration and c < viewed_time:
-            cur.execute("UPDATE programs SET duration = ? WHERE id = ?", (program.duration, id))
-        return id
-
-    cur.execute("""
-        INSERT INTO programs (event_id, service_id, name, start_time, duration, created_at)
-        VALUES (?, ?, ?, ?, ?, ?)
-    """, (
-        program.event_id,
-        program.service_id,
-        program.name,
-        program.start_time,
-        program.duration,
-        created_at
-    ))
-    return cur.lastrowid
-
-class ViewRequest(BaseModel):
-    program: Program
-    viewed_time: datetime
-
-@app.post("/api/viewed")
-def set_viewed_deprecated(data: ViewRequest, con: DbConnectionDep):
-    program_id = get_or_create_program(con, data.program, data.viewed_time, data.viewed_time)
-
-    cursor = con.cursor()
-    cursor.execute("""
-        INSERT INTO views (program_id, viewed_time, created_at)
-        VALUES (?, ?, ?)
-    """, (program_id, data.viewed_time, datetime.now()))
-    con.commit()
-    return {}
-
-class RecordRequest(BaseModel):
-    program: Program
-    file_path: str
-    recorded_at: datetime
-
-@app.post("/api/recorded")
-def set_recorded_deprecated(data: RecordRequest, con: DbConnectionDep):
-    program_id = get_or_create_program(con, data.program, data.recorded_at, data.recorded_at)
-
-    con.execute("""
-        INSERT INTO recordings (program_id, file_path, created_at)
-        VALUES (?, ?, ?)
-    """, (program_id, f"//{smb_server}{data.file_path}", data.recorded_at))
-    con.commit()
-    return {}
-
-class WatchRequest(BaseModel):
-    recording_id: int
-    move_file: bool
-
-@app.post("/api/watched")
-def set_watched_deprecated(data: WatchRequest, con: DbConnectionDep):
-    if data.move_file:
-        cur = con.execute("SELECT file_path FROM recordings WHERE id = ?", (data.recording_id,))
-        file_path, = cur.fetchone()
-        dst_file_path = smb.move_folder_by_root(file_path, dst_root)
-        con.execute("""
-            UPDATE recordings SET watched_at = ?, file_path = ? WHERE id = ?
-        """, (datetime.now(), dst_file_path, data.recording_id))
-    else:
-        con.execute("""
-            UPDATE recordings SET watched_at = ? WHERE id = ?
-        """, (datetime.now(), data.recording_id))
-
-    con.commit()
-    return {}
