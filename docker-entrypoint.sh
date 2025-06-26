1fc64303b4ce501296ed2c841a9ea4947514362a
myhilmyhill <44314051+myhilmyhill@users.noreply.github.com>
2025-06-26 22:27:04 +0900

    sqlite3

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eu
 
-python -m sqlite3 /app/db/tv.db < /app/db/schema.sql
+(cat /app/db/schema.sql; echo ".quit") | python -m sqlite3 /app/db/tv.db
 uvicorn app.main:app --host 0.0.0.0 --port $PORT
