720604ac20bcef0b240207bd0b5a49d98fa4af09
myhilmyhill <44314051+myhilmyhill@users.noreply.github.com>
2025-07-05 14:34:37 +0900

    $0

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -eux
 
 (cat /app/db/schema.sql; echo ".quit") | python -m sqlite3 /app/db/tv.db
-if [ "$1" = "dev" ]; then
+if [ "${1:-}" = "dev" ]; then
   uvicorn app.main:app --host 0.0.0.0 --port $PORT --reload --reload-dir app --log-level debug
 else
   uvicorn app.main:app --host 0.0.0.0 --port $PORT
